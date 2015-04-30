3ac68a204c10e5d159ef91c8040940423b8e4e44
Ramon De La Torre <delatorre.ramon66@gmail.com>
2015-04-30 16:16:30 -0700

    commit 13

--- js/resources.js
+++ js/resources.js
@@ -3,14 +3,21 @@ game.resources = [
      * @example
      * {name: "example", type:"image", src: "data/img/example.png"},
      */
+    //this adds a image for the background-tiles and meta tiles for the tiled application.
     {name: "background-tiles", type: "image", src: "data/img/background-tiles.png"},
     {name: "meta-tiles", type: "image", src: "data/img/meta-tiles.png"},
+    //this makes the orc and his spear show up when u press play.
     {name: "player", type: "image", src: "data/img/orcSpear.png"},
+    // this makes the image of the of the tower/castle to show up.
     {name: "tower", type: "image", src: "data/img/tower_round.svg.png"},
+    //this makes the creep  show up when it is called into the map 
     {name: "creep1", type: "image", src: "data/img/brainmonster.png"},
+    //this makes tthe title screen show upbefore the game
     {name: "title-screen", type: "image", src: "data/img/title.png"},
+    //this shows before the game starts for the player knows what to do and or the game policys. 
     {name: "exp-screen", type: "image", src: "data/img/loadpic.png"},
     {name: "gold-screen", type: "image", src: "data/img/spend.png"},
+    //the load screen shows up when the game is loading  
     {name: "load-screen", type: "image", src: "data/img/loadpic.png"},
     {name: "new-screen", type: "image", src: "data/img/newpic.png"},
     /* Atlases 
